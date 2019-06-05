a631caeef5045d4f3ba5a4a7c849db44e4276f1b
Oludayo Alli <sunnymexy@gmail.com>
2019-06-05 11:29:59 +0100

    Add project files.

---
+++ ExceptionSample/Controllers/ErrorController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace ExceptionSample.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("api/error")]
+        public IActionResult LogError()
+        {
+            //Get hold of the exceptiom that occured
+            var exFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            if(exFeature != null)
+            {
+                //Get the path where the eroor occured
+                string path = exFeature.Path;
+
+                //Get the Exception
+                Exception ex = exFeature.Error;
+
+                //Log in a flat fire or other storage
+                Log.Error(ex, path);
+
+                var error = new { ErrorMessage = ex.Message, ErrorPath = path };
+
+                return BadRequest(error);
+
+            }
+
+            return BadRequest();
+
+        }
+    }
+}
---
+++ ExceptionSample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace ExceptionSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Console()
+                .WriteTo.File("Logs\\APPNAME.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            try
+            {
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
